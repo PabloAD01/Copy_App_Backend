fe5ece79f02fbe5b4e89b858cd627034256f6f44
Pablo Araya Diaz <piarayad@gmail.com>
2023-10-09 19:50:32 -0300

    feat: connect to Database

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from 'express';
 const app = express();
+import mongoose from "mongoose";
+
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -10,6 +14,14 @@ app.get('/adbc', (req, res) => {
     res.send('Pepe!');
 })
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-})
+try {
+   await mongoose.connect(process.env.MONGO_URL);
+   app.listen(process.env.PORT, () => {
+       console.log(`Connected to database, running on port ${process.env.PORT}`);
+   })
+    
+} catch (error) {
+    console.log(error);
+  process.exit(1);
+    
+}
