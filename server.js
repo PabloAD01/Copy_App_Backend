89461bbc9a505836996439687e7235b1d40a40a4
Pablo Araya Diaz <piarayad@gmail.com>
2023-10-09 16:09:45 -0300

    feat: add express and dotenv

---
+++ server.js
@@ -0,0 +1,12 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+})
